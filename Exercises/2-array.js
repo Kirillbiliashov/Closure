1bbffbd4165d2d31d1c7616fc43c830571fb2261
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2021-09-11 10:16:36 +0300

    Implementation of tasks 1 and 2 added

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+const arr = [];
+const wrapper = (i) => arr[i];
+const methods = {
+    get: (i) => arr[i],
+    push: (val) => arr.push(val),
+    pop: () => arr.pop() 
+}
+return Object.assign(wrapper, methods)
+};
 
 module.exports = { array };
