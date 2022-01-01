7193f397a9196f0112cef0571fabd0adae3476bb
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2022-01-01 22:18:40 +0200

    Minor optimisations were added

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -4,11 +4,10 @@ const array = () => {
 const arr = [];
 const wrapper = (i) => arr[i];
 const methods = {
-    get: (i) => arr[i],
     push: (val) => arr.push(val),
     pop: () => arr.pop() 
-}
-return Object.assign(wrapper, methods)
+};
+return Object.assign(wrapper, methods);
 };
 
 module.exports = { array };
